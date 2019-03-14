d78bd56df85bbabe70b24d8861e79cf594d19caa
SNORRISS <samuel.norris@mu.edu>
2019-03-13 21:21:30 -0500

    final

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,6 @@
+var topics = ["Cat","Dog","Fox","CS:GO","Path of Exile","Tekken", "Dota", "Tetris","Sonic","Splatoon"];
+
+
 $(document).on("click", ".gif-search", function() {
   console.log("gif searching");
   var search = $(this).attr("data-search");
@@ -7,11 +10,13 @@ $(document).on("click", ".gif-search", function() {
     search +
     "&api_key=dc6zaTOxFJmzC&limit=10";
 
+
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response) {
     var results = response.data;
+    console.log(results);
 
     for (var i = 0; i < results.length; i++) {
       var newDiv = $("<div>");
@@ -19,7 +24,8 @@ $(document).on("click", ".gif-search", function() {
 
       var divImage = $("<img>");
 
-      divImage.attr("src", results[i].images.fixed_height.url);
+      divImage.attr("src", results[i].images["480w_still"].url);
+      divImage.attr("ani", results[i].images.fixed_height.url);
 
       newDiv.append(p);
       newDiv.append(divImage);
@@ -29,12 +35,41 @@ $(document).on("click", ".gif-search", function() {
   });
 });
 
-$(document).on("click", ".submit-button", function() {
-  var search = $("#search").val();
-  newButton = $("<button>");
-  newButton.attr("class", "gif-search");
-  newButton.text(search);
-  newButton.attr("data-search", search);
+$(document).on("click", "img", function() {
+  var source = $(this).attr("src");
+  var ani = $(this).attr("ani");
+  $(this).attr("src", ani);
+  $(this).attr("ani", source);
 
-  $("#buttons").append(newButton);
 });
+
+function appendButtons() {
+
+  for(var i = 0; i < topics.length; i++){
+    newButton = $("<button>");
+    newButton.attr("class", "gif-search");
+    newButton.text(topics[i]);
+    newButton.attr("data-search", topics[i]);
+    console.log(newButton);
+    $("#buttons").append(newButton);
+  }
+
+}
+
+
+
+$(document).on("click", ".submit-button", function() {
+
+  var search = $("#search-bar").val();
+  topics.push(search);
+
+  $("#buttons").html("");
+
+  
+  appendButtons();
+
+
+});
+
+
+appendButtons();
